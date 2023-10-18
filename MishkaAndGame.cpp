1e5b8df7ea540f78e6782c9aea275b4493eba13b
novalsh <muhammad.harjana@binus.ac.id>
2023-10-18 19:22:01 +0700

    MishkaAndGame

---
+++ MishkaAndGame.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int mishkaWins = 0;
+    int chrisWins = 0;
+
+    for (int i = 0; i < n; i++) {
+        int mi, ci;
+        cin >> mi >> ci;
+        if (mi > ci) {
+            mishkaWins++;
+        } else if (mi < ci) {
+            chrisWins++;
+        }
+    }
+
+    if (mishkaWins > chrisWins) {
+        cout << "Mishka" << endl;
+    } else if (mishkaWins < chrisWins) {
+        cout << "Chris" << endl;
+    } else {
+        cout << "Friendship is magic!^^" << endl;
+    }
+
+    return 0;
+}
